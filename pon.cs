ddd3c118934e2fe04ecf99c8a1f4e9fdd6a91989
daniilka29 <150164028+daniilka29@users.noreply.github.com>
2023-11-14 18:36:50 +0300

    Add files via upload

---
+++ pon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       print("здарова мужик"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
